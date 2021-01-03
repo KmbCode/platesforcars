4101c876f47763e138aa0f12e6689838cf293fb6
khalid benjelloun <khalidmailing@gmail.com>
2021-01-03 04:43:39 +0100

    d

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,45 @@
 class PagesController < ApplicationController
+	
   def index
   end
+
+  def design
+  end
+
+  def products
+  end
+
+  def accessories
+  end
+
+  def delivery
+  end
+
+  def fitting 
+  end
+
+  def faq
+  end
+
+  def reviews
+  end
+
+  def whychoose
+  end
+
+  def behind_scenes
+  end
+
+  def blog
+  end
+
+  def trade
+  end
+
+  def contact
+  end
+
+  def about
+  end
+
 end
